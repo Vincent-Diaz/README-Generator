1d06d27c3f7e182e48e09c64b3339f31be1d679d
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-13 21:45:44 -0500

    added questions for user prompts

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const markDown = generateMarkDown();
 // array of questions for user
-const questions = [
-
+const questions = [ "What is the title of this project?", "What is the desription of this project?", "What are the installation instructions?", "What will this project be used for?", "Who contributed to this project?", "What are the test instructions?"
 ];
 
 // function to write README file
