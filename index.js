a7dac52e7d1dd73bee21616a5c6d8c192b3b67dd
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-14 16:01:16 -0500

    added axios,util, writeFileAsync, and user question

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const markDown = generateMarkDown();
+const util = require("util");
+const axios = require("axios");
+const generate = require("./utils/generateMarkdown.js");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 // array of questions for user
-const questions = [ "What is the title of this project?", "What is the desription of this project?", "What are the installation instructions?", "What will this project be used for?", "Who contributed to this project?", "What are the test instructions?"
-];
+
+function promptUser() {
+    return inquirer.prompt([
+
+        {
+            type: "input",
+            message: "What is your Github username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is the title of this project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is the desription of this project?",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Provide instructions for the installation.",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "What will this project be used for?",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Who contributed to this project?",
+            name: "contributors"
+        },
+        {
+            type: "input",
+            message: "Provide instructions on how to test project.",
+            name: "test"
+        },
+        {
+            type: "list",
+            message: "Choose a license for your project.",
+            choices: [],
+            name: "license"
+        },
+    ]);
+    // console.log(data);
+    // generate(data)
+}
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+    fs.writeFile("README.md", data, err => {
+        if (err) {
+            throw err;
+        }
+        console.log("README.md file has been generated!")
+    });
 }
 
 // function to initialize program
-function init() {
-
-}
+// async function init() {
+   
+// }
 
 // function call to initialize program
 init();
