2b04cb99fac7c353e0af5530263accec8b0e13c9
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-14 21:13:45 -0500

    added try and catch to prompt func

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
-const generate = require("./utils/generateMarkdown.js");
+//const generateMarkdown = require("./utils/generateMarkdown");
+const generate = require("./utils/generateMarkdown.js")
 
 const writeFileAsync = util.promisify(fs.writeFile);
 // array of questions for user
 
 async function promptUser() {
-     const data = await inquirer.prompt([
+    try {
+        const data = await inquirer.prompt([
 
-        {
-            type: "input",
-            message: "What is your Github username?",
-            name: "username",
-            default: "Vincent Diaz"
-        },
-        {
-            type: "input",
-            message: "What is the title of this project?",
-            name: "title"
-        },
-        {
-            type: "input",
-            message: "What is the desription of this project?",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "Provide instructions for the installation.",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "What will this project be used for?",
-            name: "usage"
-        },
-        {
-            type: "input",
-            message: "Who contributed to this project?",
-            name: "contributors"
-        },
-        {
-            type: "input",
-            message: "Provide instructions on how to test project.",
-            name: "test"
-        },
-        {
-            type: "list",
-            message: "Choose a license for your project.",
-            choices: ["license1", "license2", "license3"],
-            name: "license"
-        },
-        
-    ]);
-    console.log(data)
-     
+            {
+                type: "input",
+                message: "What is your Github username?",
+                name: "username",
+                default: "Vincent Diaz"
+            },
+            {
+                type: "input",
+                message: "What is the title of this project?",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "What is the desription of this project?",
+                name: "description"
+            },
+            {
+                type: "input",
+                message: "Provide instructions for the installation.",
+                name: "installation"
+            },
+            {
+                type: "input",
+                message: "What will this project be used for?",
+                name: "usage"
+            },
+            {
+                type: "input",
+                message: "Who contributed to this project?",
+                name: "contributors"
+            },
+            {
+                type: "input",
+                message: "Provide instructions on how to test project.",
+                name: "test"
+            },
+            {
+                type: "list",
+                message: "Choose a license for your project.",
+                choices: ["license1", "license2"],
+                name: "license"
+            },
+
+        ]);
+        console.log(data)
+        writeFileAsync("README2.md", generate(data));
+    } catch (err) {
+        console.log(err);
+    }
+    // console.log(data)
+    // writeFileAsync("README2.md", generate(data));
+
     // generate(data)
 }
 
@@ -77,7 +85,7 @@ async function promptUser() {
 
 // function to initialize program
 // async function init() {
-   
+
 // }
 
 // function call to initialize program
